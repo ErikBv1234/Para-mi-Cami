fd8a1cd388ce6d31cf57bbbbc3cef9df40fec101
Erik Bravo <erikbv44@gmail.com>
2025-02-02 12:17:32 -0500

    Primer commit

--- index.py
+++ index.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/invitacion')
+def invitacion():
+    return render_template('invitacion.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
